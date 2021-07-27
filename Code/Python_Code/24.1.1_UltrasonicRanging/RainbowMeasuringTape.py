0ca3553e3561b9ea14c5eaa64738433280c8869d
jrdavidson16 <88046554+jrdavidson16@users.noreply.github.com>
2021-07-27 10:38:48 -0500

    Update and rename UltrasonicRanging.py to RainbowMeasuringTape.py

--- Code/Python_Code/24.1.1_UltrasonicRanging/RainbowMeasuringTape.py
+++ Code/Python_Code/24.1.1_UltrasonicRanging/RainbowMeasuringTape.py
@@ -8,6 +8,7 @@
 import RPi.GPIO as GPIO
 import time
 
+pins = [11, 12, 13] 
 trigPin = 16
 echoPin = 18
 MAX_DISTANCE = 220          # define the maximum measuring distance, unit: cm
@@ -37,11 +38,30 @@ def setup():
     GPIO.setmode(GPIO.BOARD)      # use PHYSICAL GPIO Numbering
     GPIO.setup(trigPin, GPIO.OUT)   # set trigPin to OUTPUT mode
     GPIO.setup(echoPin, GPIO.IN)    # set echoPin to INPUT mode
+    GPIO.setup(pins, GPIO.OUT)     # set RGBLED pins to OUTPUT mode
+    GPIO.output(pins, GPIO.HIGH)   # make RGBLED pins output HIGH level
+    pwmRed = GPIO.PWM(pins[0], 2000)      # set PWM Frequence to 2kHz
+    pwmGreen = GPIO.PWM(pins[1], 2000)  # set PWM Frequence to 2kHz
+    pwmBlue = GPIO.PWM(pins[2], 2000)    # set PWM Frequence to 2kHz
+    pwmRed.start(0)      # set initial Duty Cycle to 0
+    pwmGreen.start(0)
+    pwmBlue.start(0)
 
+def setColor(r_val,g_val,b_val):      # change duty cycle for three pins to r_val,g_val,b_val
+    pwmRed.ChangeDutyCycle(r_val)     # change pwmRed duty cycle to r_val
+    pwmGreen.ChangeDutyCycle(g_val)   
+    pwmBlue.ChangeDutyCycle(b_val)
+    
 def loop():
     while(True):
         distance = getSonar() # get distance
         print ("The distance is : %.2f cm"%(distance))
+        if distance <= 30:
+            setColor(0,100,100)
+        elif distance <= 60:
+            setColor(0,80,100)
+        elif distance <= 91:
+            setColor(0,25,100)
         time.sleep(1)
         
 if __name__ == '__main__':     # Program entrance
